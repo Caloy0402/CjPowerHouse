a74a296f0b1ed0200359453e27cb619b7fb93337
Mak Moinee <titus.silver.ph@gmail.com>
2025-10-25 22:37:17 +0800

    update

--- Admin-Dashboard.php
+++ Admin-Dashboard.php
@@ -835,8 +835,8 @@ $stmt3->close();
                             Messages</a>
                     </div>
                 </div>
-                <--?php include 'admin_notifications.php'; ?>
-                <--?php include 'admin_rescue_notifications.php'; ?>
+                <?php include 'admin_notifications.php'; ?>
+                <?php include 'admin_rescue_notifications.php'; ?>
                 <?php include 'admin_user_notifications.php'; ?>
                 <div class="nav-item dropdown">
                     <a href="" class="nav-link dropdown-toggle"
